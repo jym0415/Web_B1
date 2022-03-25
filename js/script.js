c76850128fbece5962ce5fec18e0ea245b75ce2d
jym0415 <jym9504@gmail.com>
2022-03-25 18:22:44 +0900

    대한은행

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+var px = $("#slider").width();
+var dir = -1 ;
+
+setInterval(function(){
+    $("#slider ul").animate({"marginLeft" : px * dir},400,function(){
+        $(this).find('li').eq(0).appendTo($(this));
+        $(this).css({"marginLeft" :0});
+
+    });
+},3000);
+
+$(".tab h3").click(function(){
+    $(".tab > div").removeClass('on');
+    $(this).parent().addClass('on');
+});
+
+
+
+
+});
